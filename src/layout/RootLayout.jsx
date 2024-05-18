369cb26384b435a88cc6ce381fb0424aac25cb02
Samanrashidii <samanrashidi44@gmail.com>
2024-05-18 03:46:49 -0700

    fix: show current route in header

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,10 +1,16 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation } from 'react-router-dom';
 
 function RootLayout () {
+    const location = useLocation();
+    console.log(location);
+    const pathName = location.pathname.split('/')[1];
+    const defaultPath = location.pathname === '/';
+
     return (
         <div className="layout-default">
             <header>
                 <NavLink to="/">Home</NavLink>
+                { !defaultPath && <NavLink>{pathName}</NavLink> }
             </header>
             <main>
                 <Outlet />
